ac75a25b340d67561b6b69c34dbeff45a2fa7e6d
jynn <pshenkyr@gmail.com>
2017-08-31 09:18:01 -0600

    fuck that noise

--- app/index.js
+++
@@ -1,7 +0,0 @@
-require('./styles/main.css');
-require('./styles/grid.css');
-require('modernizr/modernizr.js');
-require('riotgear/dist/rg.js');
-require('riot-bootstrap/dist/riot-bootstrap.js');
-require('./tags/app.tag');
-riot.mount('app');
