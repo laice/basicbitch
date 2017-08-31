3e318ff3040468b6346ab4db50abefa6bcd28aeb
laice <pshenkyr@gmail.com>
2017-08-31 14:47:02 -0600

    Delete 000000002.destiny2beta.js

--- src/public/posts/000000002.destiny2beta.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "title": "destiny 2 beta",
-  "date": "8/28/17",
-  "author": "jynn",
-  "text":
-    `   <p>destiny 2 pc is so lazy they didn’t even implement the keyboard entirely. twats</p>
-    `,
-  "tags": "destiny 2"
-};
