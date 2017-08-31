6f1712b12302419b6714d27e8a3b415f4be70f80
jynn <pshenkyr@gmail.com>
2017-08-31 11:50:49 -0600

    k

---
+++ src/public/posts/000000002.destiny2beta.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "title": "destiny 2 beta",
+  "date": "8/28/17",
+  "author": "jynn",
+  "text":
+    `   <p>destiny 2 pc is so lazy they didn’t even implement the keyboard entirely. twats</p>
+    `,
+  "tags": "blizzard, real id, destiny 2"
+};
