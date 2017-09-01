e38a43a083bc11d799457617911afb17a30e29df
jynn <pshenkyr@gmail.com>
2017-08-31 19:49:59 -0600

    fix data base

--- src/index.js
+++ src/index.js
@@ -179,26 +179,32 @@ https.createServer(opt, (req, res) => {
           res.write(head);
           res.write(`
           <p class="post">
-            <p>This site is written almost entirely in pure node, with the exception being the sqlite3 module, and
+            <p>
+              This site is written almost entirely in pure node, with the exception being the sqlite3 module, and
               it's dependencies.
               The reason I created the codebase behind this blog was because I was tired of having to learn bloated APIs to make
               a simple site with a REPL, and decided I just wanted to learn the barebones node way of doing this, which despite
-              what most bloated APIs would have you believe, is actually not that hard or complicated.</p>
+              what most bloated APIs would have you believe, is actually not that hard or complicated.
+            </p>
               
-              <p>I believe I've achieved an admittedly minimal but also reasonably extensible baseline from which to build my
+            <p>
+              I believe I've achieved an admittedly minimal but also reasonably extensible baseline from which to build my
               future web applications.
-               </p>
-               
-               <p>I've also wanted an easy way to get my thoughts recorded for a long time. I also have a companion
-                  cordova app for this site, available at <a href="https://github.com/laice/basicbastard"> 
-                  https://github.com/laice/basicbastard</a> - I use it to make posts
-                  from my phone. It should be fine is iOS as it isn't doing much complicated, 
-                  but I've only tested Android thus far. This is a really simple blog pipeline that's relatively easy
-                  to setup for the technically savvy.
-               </p>
-              
-              <p>If you would like to discuss these topics, or anything covered in this blog, feel free to
-              email me at <a href="mailto:jynn@badideas.today">jynn@badideas.today</a></p>
+            </p>
+            
+            <p>
+              I've also wanted an easy way to get my thoughts recorded for a long time. I also have a companion
+              cordova app for this site, available at <a href="https://github.com/laice/basicbastard"> 
+              https://github.com/laice/basicbastard</a> - I use it to make posts
+              from my phone. It should be fine is iOS as it isn't doing much complicated, 
+              but I've only tested Android thus far. This is a really simple blog pipeline that's relatively easy
+              to setup for the technically savvy.
+            </p>
+            
+            <p>
+              If you would like to discuss these topics, or anything covered in this blog, feel free to
+              email me at <a href="mailto:jynn@badideas.today">jynn@badideas.today</a>
+            </p>
           </p>
           <p class="post">
             <h3>Source:</h3>
